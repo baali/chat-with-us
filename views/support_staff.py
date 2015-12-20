e8f933580840122db9498772b9160a6aac5d0f05
baali <baali@muse-amuse.in>
2015-12-20 17:15:32 +0530

    Placeholder python script to list support profiles.

---
+++ views/support_staff.py
@@ -0,0 +1,5 @@
+# List of support email ids which you want to use for chatting with
+# customers something like:
+# support_ids = ["support-1@domain.com",
+#                "support-2@domain.com"]
+support_ids = []
